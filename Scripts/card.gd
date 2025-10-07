0fedac8a24506b03cc5ac57aa8c5790c725fed3b
ZaneSoCool <zanenicholaspederson@gmail.com>
2025-10-07 12:51:07 -0500

    Revised card scene/script +re-organized files
    ^

---
+++ Scripts/card.gd
@@ -0,0 +1,32 @@
+extends Node2D
+
+#Made by Kian edited by Zane
+
+@onready var textureRect: TextureRect = $TextureRect
+@onready var label: Label = $Label
+
+var card_id : int #1,2.. 11 (Jack), 12 (Queen), 13 (King)
+var card_suite : int #1, 2, 3, 4
+
+var id_text_dict = {
+	1 : "res://icon.svg",
+	2 : "res://icon.svg",
+	3 : "res://icon.svg",
+	4 : "res://icon.svg",
+	5 : "res://icon.svg",
+	6 : "res://icon.svg",
+	7 : "res://icon.svg",
+	8 : "res://icon.svg",
+	9 : "res://icon.svg",
+	10 : "res://icon.svg",
+	11 : "res://icon.svg",
+	12 : "res://icon.svg",
+	13 : "res://icon.svg"
+}
+
+func _ready() -> void:
+	if card_id == 0: #if card's id isnt setup it destoys itself
+		queue_free()
+		
+	textureRect.texture = id_text_dict[card_id]
+	label.text = str(card_id)
--- Scripts/menuscript.gd
+++ Scripts/menuscript.gd
@@ -1,5 +1,7 @@
 extends Control
 
+#Made by Henry
+
 func _on_button_pressed() -> void:
 	#Make sure to add starting scene from here
 	get_tree().change_scene_to_file("res://(INSERT_START_SCENE_HERE).tscn")
