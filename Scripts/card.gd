07dde190c17700985d98d06694e4522d4cc1e3a1
ZaneSoCool <zanenicholaspederson@gmail.com>
2025-10-07 12:55:27 -0500

    +suite overlay texture in card

--- Scripts/card.gd
+++ Scripts/card.gd
@@ -2,7 +2,8 @@ extends Node2D
 
 #Made by Kian edited by Zane
 
-@onready var textureRect: TextureRect = $TextureRect
+@onready var card_texture: TextureRect = $Card_Texture
+@onready var suite_texture: TextureRect = $Suite_Texture
 @onready var label: Label = $Label
 
 var card_id : int #1,2.. 11 (Jack), 12 (Queen), 13 (King)
@@ -24,9 +25,18 @@ var id_text_dict = {
 	13 : "res://icon.svg"
 }
 
+var suite_text_dict = {
+	1 : "res://icon.svg",
+	2 : "res://icon.svg",
+	3 : "res://icon.svg",
+	4 : "res://icon.svg",
+}
+
 func _ready() -> void:
 	if card_id == 0: #if card's id isnt setup it destoys itself
 		queue_free()
 		
-	textureRect.texture = id_text_dict[card_id]
+	card_texture.texture = id_text_dict[card_id]
+	suite_texture.texture = suite_text_dict[card_id]
+	
 	label.text = str(card_id)
