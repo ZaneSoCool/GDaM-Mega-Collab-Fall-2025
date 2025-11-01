4cbdccbc40e7bf25d808ae39ae00099e49caaa31
ZaneSoCool <zanenicholaspederson@gmail.com>
2025-11-01 17:00:02 -0500

    uh oh :(
    things did not go well, gave up on the current implementation of readSlots

--- Scripts/slotMachine.gd
+++ Scripts/slotMachine.gd
@@ -36,31 +36,10 @@ func readSlots():
 	realSymbols.remove_at(4)
 	
 	for symbol in realSymbols: #loop through each of the 4 symbols
-
-		for slot in slots[0]:
-			var validSlots1=[] #slots in column 2 which will be valid according to column 1
-			
-			if slot.slotGraphic.texture == symbol or slot.slotGraphic.texture == symbols[4]:
-				var slotIndex1 = slots[0].find(slot)
-				validSlots1.append(slots[1][slotIndex1])
-				if slotIndex1 - 1 >= 0:
-					validSlots1.append(slots[1][slotIndex1-1])
-				if slotIndex1 + 1 <= 2:
-					validSlots1.append(slots[1][slotIndex1+1])
-					
-				for slot2 in validSlots1:
-					var validSlots2=[] #slots in column 3 which will be valid according to column 2
-			
-					if slot2.slotGraphic.texture == symbol or slot2.slotGraphic.texture == symbols[4]:
-						var slotIndex2 = slots[2].find(slot2)
-						validSlots2.append(slots[2][slotIndex2])
-						if slotIndex2 - 1 >= 0:
-							validSlots2.append(slots[2][slotIndex2-1])
-						if slotIndex2 + 1 <= 2:
-							validSlots2.append(slots[2][slotIndex2+1])
-							
-							for slot3 in validSlots2:
-								if slot3.slotGraphic.texture == symbol or slot3.slotGraphic.texture == symbols[4]:
-									wins += 1
-									break
+		pass #TODO: FIGURE OUT QUANTIY OF WINS
+		
 	print(wins)
+	giveRewards(wins)
+	
+func giveRewards(wins : int):
+	pass
