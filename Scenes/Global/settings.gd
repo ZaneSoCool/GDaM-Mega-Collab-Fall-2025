92813c86e619b991e996898d6d0f8af620dc5cfc
ZaneSoCool <zanenicholaspederson@gmail.com>
2025-11-13 17:22:27 -0600

    display settings and fullscreen button

--- Scenes/Global/settings.gd
+++ Scenes/Global/settings.gd
@@ -2,12 +2,18 @@ extends Control
 
 @onready var foldable_container: FoldableContainer = $FoldableContainer
 @onready var confirmation_dialog: ConfirmationDialog = $FoldableContainer/VBoxContainer/Button/ConfirmationDialog
+@onready var fullscreen_button: CheckButton = $FoldableContainer/VBoxContainer/FullscreenButton
 
 #made by Zane Pederson
 
 func _ready() -> void:
 	foldable_container.folded = true
 	confirmation_dialog.visible = false
+	
+	if DisplayServer.window_get_mode() == 4:
+		fullscreen_button.button_pressed = true
+	else:
+		fullscreen_button.button_pressed = false
 	
 func _on_button_pressed() -> void:
 	confirmation_dialog.visible = true
@@ -18,3 +24,9 @@ func _on_confirmation_dialog_canceled() -> void:
 	
 func _on_confirmation_dialog_confirmed() -> void:
 	SceneTransition.change_scene_to("res://Scenes/Menu.tscn")
+
+func _on_check_button_pressed() -> void:
+	if fullscreen_button.button_pressed == true:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN)
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_MAXIMIZED)
--- Scripts/menuscript.gd
+++ Scripts/menuscript.gd
@@ -4,8 +4,7 @@ extends Control
 
 func _on_button_pressed() -> void:
 	#Make sure to add starting scene from here
-	get_tree().change_scene_to_file("res://(INSERT_START_SCENE_HERE).tscn")
-
+	SceneTransition.change_scene_to("res://Scenes/levels/1_1.tscn")
 
 func _on_button_2_pressed() -> void:
 	get_tree().quit()
