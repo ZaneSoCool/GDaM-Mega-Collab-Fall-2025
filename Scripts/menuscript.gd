67710274ccee9de949d1e34b95d8455bee0e58bd
ZaneSoCool <zanenicholaspederson@gmail.com>
2025-11-15 15:56:17 -0600

    added credits menu (names still needed)

--- Scripts/menuscript.gd
+++ Scripts/menuscript.gd
@@ -8,3 +8,6 @@ func _on_button_pressed() -> void:
 
 func _on_button_2_pressed() -> void:
 	get_tree().quit()
+
+func _on_credits_button_pressed() -> void:
+	SceneTransition.change_scene_to("res://Scenes/credits.tscn")
